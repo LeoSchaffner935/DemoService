312ab8a1972c7a38abb0fdd6fa862d4d28555439
Leo Schaffner <leoschaffner935@gmail.com>
2022-06-08 13:03:04 -0400

    test commit for jenkins

--- src/main/java/com/revature/DemoServiceApplication.java
+++ src/main/java/com/revature/DemoServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoServiceApplication {
-
+// This is a change to test jenkins auto-build
 	public static void main(String[] args) {
 		SpringApplication.run(DemoServiceApplication.class, args);
 	}
